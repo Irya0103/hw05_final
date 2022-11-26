49e17e4729f8fd932e24be642402a90609152251
unknown <minuto_suerte@mail.ru>
2022-11-26 13:22:19 +0300

    ..

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -70,7 +70,7 @@ class Follow(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='following')
-    
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
