4cad16c06b64efc20f32c617e3f2b6b06f5cab1e
Ира Андреева <minuto_suerte@mail.ru>
2022-12-02 11:27:20 +0300

    ..

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -214,7 +214,8 @@ class PaginatorViewsTest(TestCase):
     def test_second_profile_page_contains_three_records(self):
         """Количество постов на второй странице ровно 3"""
         url = reverse(
-            'posts:profile', kwargs={'username': self.user.username}) + '?page=2'
+            'posts:profile',
+            kwargs={'username': self.user.username}) + '?page=2'
         response = self.guest_client.get(url)
         self.assertEqual(len(response.context['page_obj']), 3)
 
